d2f81529c35de2cbab5771e8fc19b35f53a8f620
lawranenicss <77539897+lawranenicss@users.noreply.github.com>
2022-01-14 13:32:45 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,13 @@
 makeCacheMatrix <- function(x = numeric()) {
 ## nothing is cached at first so we set N to NULL
 N <- NULL
-## next store a matrix
-setMatrix <- function(newValue) {
-x <<- newValue
-## since the matrix is assigned a new value, flush the cache N
+## next store the inverse of the matrix
+set <- function(Inverse) {
+x <<- Inverse
 N <<- NULL
 }
-## this will return the stored matrix
-getMatrix <- function() {
+## this will return the inverse of the matrix
+getInverse <- function() {
 x
 }
 cacheInverse <- function(solve) {
@@ -21,19 +20,19 @@ N
 }
 ## return a list
 ## each element is a function for the matrix
-list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+list(set = set, getInverse = getInverse, cacheInverse = cacheInverse)
 }
-## cacheSolve function will solve for the inverse of the matrix if it does not exist
-cacheSolve <- function(y, ...) {
+## solve function will solve for the inverse of the matrix if it does not exist
+solve <- function(a, ...) {
 ## here we take the cached value and if the inverse is found the value is returned
-inverse <- y$getInverse()
+inverse <- a$getInverse()
 if(!is.null(inverse)) {
-message("getting cached data")
+message("retrieving inverse")
 return(inverse)
 }
 ## if the inverse is not found we take the matrix and solve for the value
-data <- y$getMatrix()
+data <- a$getInverse()
 inverse <- solve(data)
-y$cacheInverse(inverse)
+a$cacheInverse(inverse)
 inverse
 }
